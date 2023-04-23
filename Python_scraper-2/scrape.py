fd67bc757f64c9a7beb588e0f6e2657480c51bac
Pviksy <birkegaardm@gmail.com>
2023-04-23 21:18:14 +0200

    Deleted Python_scraper-2. Decided to update original project instead

--- Python_scraper-2/scrape.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-
-def get_soup(url):
-    req = requests.get(url)
-    html = req.content
-    soup = BeautifulSoup(html, 'html5lib')
-    return soup
